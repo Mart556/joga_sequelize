4b26fd69814e79a639c5bb993acfc6b22b45a799
Mart556 <mart.haamer@voco.ee>
2025-09-18 13:08:18 +0300

    Created connection to database

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+import { Sequelize } from "sequelize";
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASS,
+	{
+		host: process.env.DB_HOST,
+		dialect: "mysql",
+	}
+);
+
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log("Connection has been established successfully.");
+	})
+	.catch((err) => {
+		console.error("Unable to connect to the database:", err);
+	});
 
 app.get("/", (req, res) => {
 	res.send("Hello, World!");
